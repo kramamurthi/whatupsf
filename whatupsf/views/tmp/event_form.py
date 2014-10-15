262c245da321065f93881c2a7b075cb565a7333a
Alex Pandian <alexpandian@me.com>
2014-10-15 01:29:49 -0700

    cleanup

--- whatupsf/views/tmp/event_form.py
+++
@@ -1,7 +0,0 @@
-def save_event(request):  
-    form = BookForm
-    if request.POST:
-        form = BookForm(request.POST)
-    if form.is_valid():
-        form.save()
-    return render_to_response('event_entry.html',{'form': form}, context_instance=RequestContext(request))
