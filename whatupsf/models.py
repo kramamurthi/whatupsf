60dddb957693684ae04e40172cb06671bfa19df0
Alex Pandian <alexpandian@me.com>
2014-10-16 17:29:36 -0700

    Clean up models.py and mysql db in sync with it

--- whatupsf/models.py
+++ whatupsf/models.py
@@ -1,25 +1,181 @@
-# Import the Django models ORM library                                                                                
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [app_label]'
+# into your database.
+from __future__ import unicode_literals
+
 from django.db import models
-from django import forms
-from datetime import date, datetime
-# Create a model class to store the data for                                                                          
-# T-Shirt registration. Various types of fields                                                                       
-# are available in the models package.                                                                                
-#class TShirtRegistration(models.Model):                                                                               
-#    name = models.CharField(max_length=100)                                                                             
-#    email = models.EmailField()                                                                                         
-#    tshirt_sizes = (('S', 'Small'),                                                                                     
-#    ('M', 'Medium'),                                                                                    
-#    ('L', 'Large'),                                                                                     
-#    ('XL', 'Extra Large'))                                                                              
-# Limit the choices for the size field to the above given choices                                                   
-#    size = models.CharField(max_length=2, choices=tshirt_sizes)                                                         
-#    address = models.TextField()          
+
+
+class AuthGroup(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    group_id = models.IntegerField()
+    permission_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+
+
+class AuthMessage(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    user_id = models.IntegerField()
+    message = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_message'
+
+
+class AuthPermission(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(max_length=50)
+    content_type_id = models.IntegerField()
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+
+
+class AuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField()
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=75)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    user_id = models.IntegerField()
+    group_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    user_id = models.IntegerField()
+    permission_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+
+
+class DjangoAdminLog(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    action_time = models.DateTimeField()
+    user_id = models.IntegerField()
+    content_type_id = models.IntegerField(blank=True, null=True)
+    object_id = models.TextField(blank=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.IntegerField()
+    change_message = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(max_length=100)
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+
+
+class DjangoMigrations(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Geoloc(models.Model):
+    address = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    statezip = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    id = models.IntegerField(primary_key=True)  # AutoField?
+
+    class Meta:
+        managed = False
+        db_table = 'geoloc'
+
+
+class Venues(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    zip = models.IntegerField(blank=True, null=True)
+    phone = models.CharField(max_length=12, blank=True)
+    url = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'venues'
+
 
 class EventInformation(models.Model):
-    eventName = models.CharField(max_length=100)
-    eventPrice = models.DecimalField(max_digits=4, decimal_places=0)
-    #eventTime = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
-    eventDate = models.DateField(default=date.today())
-    eventTime = models.TimeField(default=datetime.now())
-    eventUrl = models.URLField(max_length=255)
+    #id = models.IntegerField(primary_key=True)  # AutoField?
+    eventName = models.CharField(db_column='eventName', max_length=100)  # Field name made lowercase.
+    eventPrice = models.DecimalField(db_column='eventPrice', max_digits=4, decimal_places=0)  # Field name made lowercase.
+    eventDate = models.DateField(db_column='eventDate')  # Field name made lowercase.
+    #eventTime = models.DateTimeField(db_column='eventTime')  # Field name made lowercase.
+    eventUrl = models.CharField(db_column='eventUrl', max_length=255)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'whatupsf_eventinformation'
