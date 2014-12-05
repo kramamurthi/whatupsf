c2eefd67946417d9b5868d71da419fc73cae9698
Alex Pandian <alexpandian@me.com>
2014-12-04 22:45:33 -0800

    ETL helpers added

--- etl/venueETL.py
+++ etl/venueETL.py
@@ -1,5 +1,8 @@
 import MySQLdb as msq
 from geopy import geocoders
+from collections import OrderedDict
+import json
+from firebase import firebase
 
 def getListOfAddresses(dbName, tblName):
 
@@ -69,5 +72,75 @@ def get_json_data(dbName, tblName):
     finally:
         db.close()
 
+def dictfetchall(cursor):
+    "Returns all rows from a cursor as a dict"
+    desc = cursor.description
+    return [OrderedDict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
+
+def get_table_json(dbName, tblName):
+
+    db = msq.connect("mysql.whatupsf.com", "kramamurthi", "dream2Win", dbName)
+    cursor = db.cursor()
+    sql = "SELECT * from %s" %(tblName)
+    try:
+        cursor.execute(sql)
+        jsonList = dictfetchall(cursor)
+    except:
+        print "Error: unable to execute query"
+    else:
+        return jsonList
+    finally:
+        db.close()
+
+                                            
+def dump_table_json(dbName, tblName):
+
+    jData = get_table_json(dbName, tblName)
+    jString = json.dumps(jData, sort_keys = False, indent=4)
+    fH = open(tblName + ".json","w")
+    fH.write(jString)
+    fH.close()
+
+def get_events(dump=False):
+
+    eventList = []
+    fire = firebase.FirebaseApplication('https://popping-fire-3129.firebaseio.com/')
+    jData  = fire.get('', None)
+    if dump:
+        jString = json.dumps(jData, sort_keys = False, indent=4)
+        fH = open("events.json","w")
+        fH.write(jString)
+        fH.close()
+    for venue in jData:
+        for event in venue[u'events']:
+            if event[u'eventName'] != u'':
+                eventList.append(event)
+
+    return eventList
+    #json_str = json.dumps(json_data)
 
 
+def ingest_bands(dbName, tblName):
+    db = msq.connect("mysql.whatupsf.com", "kramamurthi", "dream2Win", dbName)
+    cursor = db.cursor()
+    events_data = get_events()  #Today's existing events
+
+    # Get Unique Existing Bands in Table
+    sql = "SELECT * from %s" %(tblName)
+    print "Query: " + sql
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        bandList = [] 
+        for row in rows:
+            bandList.append(row[1])
+    except:
+        print "Error: unable to execute read query"
+    else:
+        for event in events_data:
+            if event[u'eventName'] not in bandList:
+                sql = """INSERT into %s(name, media_url) VALUES("%s", "%s")""" %(tblName,event[u'eventName'], event[u'eventUrl'])
+                print sql
+                cursor.execute(sql)
+    finally:
+        db.close()
